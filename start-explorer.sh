e7fe50ac474dc37a990b2e3e5de6c915196347f6
Infinity Software Ltd <31319908+Plusid@users.noreply.github.com>
2020-11-15 14:52:51 +0400

    Delete start-explorer.sh

--- start-explorer.sh
+++
@@ -1,6 +0,0 @@
-NETWORK="$1"
-if [ -z "$NETWORK" ]; then
-    NETWORK="devnet"
-fi
-HOST="" PORT="" yarn build:"$NETWORK"
-EXPLORER_HOST="68.183.102.212" EXPLORER_PORT="4200" pm2 start /home/lionel/core-explorer/express-server.js --name explorer
